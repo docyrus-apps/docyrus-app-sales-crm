90f75fe80cd7a982c69ef8d47f2099c4ba937b69
Anıl Beyazoglu <anil@beyazoglu.com>
2026-03-03 00:22:51 +0300

    fix: iframe login

--- vite.config.ts
+++ vite.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
     allowedHosts: ['.e2b.dev', '.docy.app', '.docy.dev', '.docyrus.app'],
     headers: {
       'Content-Security-Policy':
-        "frame-ancestors 'self' https://localhost:4200 http://localhost:5173 http://localhost:3000 https://localhost:3000 https://alpha.build.docyrus.app https://build.docyrus.app https://docyrus.app https://alpha.build.docy.app https://docy.app",
+        "frame-ancestors 'self' https://localhost:4200 http://localhost:5173 http://localhost:3000 https://localhost:3000 https://studio.docyrus.app https://docyrus.app https://next.docyrus.app https://docy.app",
     },
   },
 })
